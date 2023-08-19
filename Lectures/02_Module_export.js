952690abf472834c4f62f659b5b253f77b2ff26e
Devendra Dhakad <121676745+Devendradhakad12@users.noreply.github.com>
2023-08-19 11:01:19 +0530

    feat

--- Lectures/02_Module_export.js
+++ Lectures/02_Module_export.js
@@ -6,6 +6,8 @@
 
 const sum = (a,b)=> a+b
 const mul = (a,b)=> a*b
+export const sub = (a,b)=>a-b
 const PI = 3.14
 
-module.exports = {sum , mul , PI}
+//module.exports = {sum , mul , PI}  // for do module.exports remove "type":"module" from package.json
+
---
+++ Lectures/index.js
@@ -0,0 +1,3 @@
+import {sub} from './02_Module_export.js';
+
+console.log(sub(3,2))
